7fcb53953d6509396ea44ff32892e03e1515df6a
codewithjaya <jayamukherjee.csus@gmail.com>
2025-08-29 06:58:29 -0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (50.74%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,25 +1,43 @@
 class Solution {
-    public int climbStairs(int n) {
-        int memo[] = new int[n+1];
-        return climbStairs_rec(0, n, memo);
-    }
+//     public int climbStairs(int n) {
+//         int memo[] = new int[n+1];
+//         return climbStairs_rec(0, n, memo);
+//     }
 
-public int climbStairs_rec(int i, int n, int[] memo)
-{
-    if(i>n)
-    {
-        return 0;
-    }
-    if(i == n)
+// public int climbStairs_rec(int i, int n, int[] memo)
+// {
+//     if(i>n)
+//     {
+//         return 0;
+//     }
+//     if(i == n)
+//     {
+//         return 1;
+//     }
+//     if(memo[i] > 0)
+//     {
+//         return memo[i];
+//     }
+//     memo[i] = climbStairs_rec(i+1,n, memo) + climbStairs_rec(i+2,n, memo);
+// return memo[i] ;
+// }
+
+//DP
+public int climbStairs(int n) {
+    if(n==1)
     {
         return 1;
     }
-    if(memo[i] > 0)
+
+    int[] dp = new int[n +1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i=3; i <= n; i++)
     {
-        return memo[i];
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    memo[i] = climbStairs_rec(i+1,n, memo) + climbStairs_rec(i+2,n, memo);
-return memo[i] ;
+
+    return dp[n];
 }
         
     }
